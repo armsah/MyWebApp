11fb71822d72c68202b57eadb3550e452089d7df
armsah <sahakyanarmen5@gmail.com>
2025-10-31 18:45:28 +0200

    Trigger CI/CD deployment

--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3de20b0a1b394d7fe7244dc3aabc537be47877e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
